1706e7266f135a4954217f0f7927a8ece316676e
Debasish Bhuyan <debasishbhuyan280@gmail.com>
2026-01-06 17:53:03 +0000

    Maximum Path of a Binary Tree

---
+++ BinaryTreeMaxPath.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxSum;
+
+    int dfs(TreeNode* root) {
+        if (!root) return 0;
+
+        int left = max(0, dfs(root->left));
+        int right = max(0, dfs(root->right));
+
+        maxSum = max(maxSum, root->val + left + right);
+
+        return root->val + max(left, right);
+    }
+
+    int maxPathSum(TreeNode* root) {
+        maxSum = INT_MIN;
+        dfs(root);
+        return maxSum;
+    }
+};
+
+int main() {
+
+    TreeNode* root = new TreeNode(-10);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    Solution sol;
+    cout << "Maximum Path Sum: " << sol.maxPathSum(root) << endl;
+
+    return 0;
+}
